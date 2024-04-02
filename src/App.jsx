cabb5528f074ea80ef7c9c6ef5af7ce88c6f39b9
JiHye Yoon <yoonjihyeoi@gmail.com>
2024-04-02 11:45:26 -0700

    complete code review

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,19 @@
 import "./App.css";
-import EightBall from "./EightBall";
+import EightBall from "./EightBall.jsx";
 
-const POSSIBLE_ANSWERS = [
-  {msg: "It is certain.", color: "green"},
-  {msg: "It is decidedly so.", color: "green"},
-  {msg: "Without a doubt.", color: "green"},
-  {msg: "Yes - definitely.", color: "green"},
-  {msg: "You may rely on it.", color: "green"},
-  {msg: "As I see it, yes.", color: "green"},
-  {msg: "Most likely.", color: "green"},
-  {msg: "Outlook good.", color: "green"},
-  {msg: "Yes.", color: "green"},
-  {msg: "Signs point to yes.", color: "goldenrod"},
-  {msg: "Reply hazy, try again.", color: "goldenrod"},
-  {msg: "Ask again later.", color: "goldenrod"},
-  {msg: "Better not tell you now.", color: "goldenrod"},
-  {msg: "Cannot predict now.", color: "goldenrod"},
-  {msg: "Concentrate and ask again.", color: "goldenrod"},
-  {msg: "Don't count on it.", color: "red"},
-  {msg: "My reply is no.", color: "red"},
-  {msg: "My sources say no.", color: "red"},
-  {msg: "Outlook not so good.", color: "red"},
-  {msg: "Very doubtful.", color: "red"},
-];
 
-/** Component for entire page.
+
+/** App renders EightBall
  *
- * Props: none
- * State: none
+ * Props: None
+ * State: None
  *
- */
+ * App --> EightBall
+ *
+*/
 
 function App() {
-  return <EightBall answers={POSSIBLE_ANSWERS} />;
+  return <EightBall />;
 }
 
 export default App;
--- src/eightball.js
+++ src/eightball.js
@@ -1,6 +1,12 @@
 "use strict";
 
-/** */
-function choice() {
+/**
+ * returns a random number between 0 and maxValue
+*/
 
+function choice(maxValue) {
+  const randIdx = Math.floor(Math.random() * maxValue);
+  return randIdx;
 }
+
+export { choice };
